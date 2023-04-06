73fafa7135b380040bd3bacbf775fc52dfdccf74
uhdking <124318949+uhdking@users.noreply.github.com>
2023-04-06 06:37:19 +0530

    Update info.py

--- utils/info.py
+++ utils/info.py
@@ -1,21 +1,3 @@
-#=========================================================================
-# [AutoDelete - Telegram bot to delete messages after specific time]      
-# Copyright (C) 2022 Arunkumar Shibu                       
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#=========================================================================
-
 import os
 
 API_ID       = int(os.environ.get("API_ID", "9453727"))
